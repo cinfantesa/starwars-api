e9243725fae856ebf2d68e843cb003c75364a066
Cristobal Infantes <cinfantes@grupoasv.com>
2017-01-02 10:54:51 +0100

    Inicializando repositorio

---
+++ src/main/java/com/starwars/StarwarsApiApplication.java
@@ -0,0 +1,12 @@
+package com.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsApiApplication.class, args);
+	}
+}
