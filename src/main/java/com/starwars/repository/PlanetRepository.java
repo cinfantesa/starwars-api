a23b7dc49da88bd09fa11ffb18680e7586f762a3
Cristobal Infantes <cinfantes@grupoasv.com>
2017-03-13 22:59:04 +0100

    using @param on planetrepository

--- src/main/java/com/starwars/repository/PlanetRepository.java
+++ src/main/java/com/starwars/repository/PlanetRepository.java
@@ -2,6 +2,7 @@ package com.starwars.repository;
 
 import com.starwars.model.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface PlanetRepository extends JpaRepository<Planet,Long>{
     Planet findByName(String name);
-    List<Planet> findByNameContaining(String name);
+    List<Planet> findByNameContaining(@Param("name") String name);
     List<Planet> findAllByOrderByNameDesc();
     List<Planet> findByPopulationGreaterThan(Long population);
     List<Planet> findByPopulationBetween(Long min, Long max);
