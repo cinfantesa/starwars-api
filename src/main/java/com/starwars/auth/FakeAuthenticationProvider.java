77a480583563af99086745b2ed74affaf465cbae
Cristobal Infantes <cinfantes@grupoasv.com>
2017-03-30 19:43:24 +0200

    actualizada configuración

--- src/main/java/com/starwars/auth/FakeAuthenticationProvider.java
+++ src/main/java/com/starwars/auth/FakeAuthenticationProvider.java
@@ -8,9 +8,11 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Service
 public class FakeAuthenticationProvider implements AuthenticationProvider{
@@ -26,19 +28,25 @@ public class FakeAuthenticationProvider implements AuthenticationProvider{
     String name = authentication.getName();
     Account account = accountRepository.findByUsername(name);
 
-    if (account == null) {
-      throw new BadCredentialsException("User not exists");
+    if (account == null || !authentication.getCredentials().equals(account.getPassword())) {
+      throw new BadCredentialsException("User not exists or incorrect password");
     }
 
-    if (name.equals(account.getPassword())) {
-      return new UsernamePasswordAuthenticationToken(name, account.getPassword(), new ArrayList<>());
-    }
-
-    throw new BadCredentialsException("Incorrect Password");
+    return new UsernamePasswordAuthenticationToken(name, account.getPassword(), getGrantedAuthorities(name));
   }
 
   @Override
   public boolean supports(Class<?> aClass) {
     return UsernamePasswordAuthenticationToken.class.isAssignableFrom(aClass);
+  }
+
+  private Collection<? extends GrantedAuthority> getGrantedAuthorities(String username) {
+    Collection<? extends GrantedAuthority> authorities;
+    if ("admin".equals(username)) {
+      authorities = Arrays.asList(() -> "ROLE_ADMIN", () -> "ROLE_BASIC");
+    } else {
+      authorities = Arrays.asList(() -> "ROLE_BASIC");
+    }
+    return authorities;
   }
 }
