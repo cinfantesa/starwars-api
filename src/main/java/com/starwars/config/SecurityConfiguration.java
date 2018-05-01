d579afe3694dcf24a059906e8497e4ef9495e1ad
Cristobal Infantes <cinfantes@grupoasv.com>
2018-05-01 12:32:25 +0200

    enabling secured & prepost

--- src/main/java/com/starwars/config/SecurityConfiguration.java
+++ src/main/java/com/starwars/config/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.starwars.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @AllArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final FakeAuthenticationProvider fakeAuthenticationProvider;
 
